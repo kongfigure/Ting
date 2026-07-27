b0878f4162392ace01fa1ee3ce26a3716eeb5510
Nicole Kong <mningk@gmail.com>
2026-07-27 16:17:55 -0700

    Initial Commit

---
+++ Ting/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ting
+//
+//  Created by Nicole Kong Mei Ning on 27/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ting/TingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TingApp.swift
+//  Ting
+//
+//  Created by Nicole Kong Mei Ning on 27/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct TingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
